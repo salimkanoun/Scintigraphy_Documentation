c4ecc1355c77e49fffa6866d14a63538db5eebc7
Helyion <gaelbellanger283@gmail.com>
2022-04-13 16:30:18 +0200

    changement css

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -57,8 +57,10 @@ function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
+        <div className={styles.color}>
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
