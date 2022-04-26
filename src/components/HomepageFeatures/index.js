2060da69fc0d7a7e8ea915819091d884243da28a
Helyion <gaelbellanger283@gmail.com>
2022-04-26 15:25:27 +0200

    correction total du code de shijie pour le re-rendre modulable

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -12,8 +12,6 @@ const FeatureList = [
     ),
     lien: 'docs/Preparation/Download',
   },
-];
-const FeatureList1 = [
   {/*we&apos;ll*/
     title: 'Useful everyday applications',
     description: (
@@ -21,10 +19,8 @@ const FeatureList1 = [
         Many applications available...
       </>
     ),
-    lien: 'docs/Preparation/Download',
+    lien: 'docs/Preparation/Installation',
   },
-];
-const FeatureList2 = [
   {
     title: 'Original tools',
     description: (
@@ -34,8 +30,6 @@ const FeatureList2 = [
     ),
     lien: 'docs/Preparation/Download',
   },
-];
-const FeatureList3 = [
   {
     title: 'Updated applications according to the latest guidelines',
     description: (
@@ -45,8 +39,6 @@ const FeatureList3 = [
     ),
     lien: 'docs/Preparation/Download',
   },
-];
-const FeatureList4 = [
   {
     title: 'Available in Open source!',
     description: (
@@ -56,8 +48,6 @@ const FeatureList4 = [
     ),
     lien: 'docs/Preparation/Download',
   },
-];
-const FeatureList5 = [
   {
     title: 'Possible evolutions and new tools to be developed!',
     description: (
@@ -69,90 +59,20 @@ const FeatureList5 = [
   },
 ];
 
-function Feature({Svg, title, description,lien}) {
+function Feature({title, description,lien}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center padding-horiz--md text--primary">
         <div className={styles.color}>
           <h3>{title}</h3>
           <p>{description}</p>
-          
-          <a href="http://localhost:3000/docs/Preparation/Download">+ Learn more</a>.
+          <a href={lien}>+ Learn more</a>
         </div>
       </div>
     </div>
   );
 }
-function Feature1({Svg, title, description,lien}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md text--primary">
-        <div className={styles.color}>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          
-          <a href="http://localhost:3000/docs/intro">+ Learn more</a>.
-        </div>
-      </div>
-    </div>
-  );
-}
-function Feature2({Svg, title, description,lien}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md text--primary">
-        <div className={styles.color}>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          
-          <a href="http://localhost:3000/our_services">+ Learn more</a>.
-        </div>
-      </div>
-    </div>
-  );
-}
-function Feature3({Svg, title, description,lien}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md text--primary">
-        <div className={styles.color}>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          
-          <a href="http://localhost:3000/docs/Preparation/Download">+ Learn more</a>.
-        </div>
-      </div>
-    </div>
-  );
-}
-function Feature4({Svg, title, description,lien}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md text--primary">
-        <div className={styles.color}>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          
-          <a href="https://github.com/facebook/docusaurus">+ Learn more</a>.
-        </div>
-      </div>
-    </div>
-  );
-}
-function Feature5({Svg, title, description,lien}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center padding-horiz--md text--primary">
-        <div className={styles.color}>
-          <h3>{title}</h3>
-          <p>{description}</p>
-          
-          <a href="http://localhost:3000/contact_us">+ Learn more</a>.
-        </div>
-      </div>
-    </div>
-  );
-}
+
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
@@ -160,21 +80,6 @@ export default function HomepageFeatures() {
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
-          ))}
-            {FeatureList1.map((props, idx) => (
-            <Feature1 key={idx} {...props} />
-          ))}
-            {FeatureList2.map((props, idx) => (
-            <Feature2 key={idx} {...props} />
-          ))}
-            {FeatureList3.map((props, idx) => (
-            <Feature3 key={idx} {...props} />
-          ))}
-            {FeatureList4.map((props, idx) => (
-            <Feature4 key={idx} {...props} />
-          ))}
-            {FeatureList5.map((props, idx) => (
-            <Feature5 key={idx} {...props} />
           ))}
         </div>
       </div>
