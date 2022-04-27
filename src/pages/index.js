6043d982c0abd43ff65a1fd8766082cf70643648
Helyion <gaelbellanger283@gmail.com>
2022-04-27 10:28:32 +0200

    ajout code shijie

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,39 @@
 import React from 'react';
+import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-/*import Link from '@docusaurus/Link';
-import clsx from 'clsx';*/
+import DocusaurusSvg from '/static/img/appareil.jpg';
+import Icone from '/static/img/iconeNucle.png';
+
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header >
-       <div className={styles.avantAppli1} ></div>
-     <div className={styles.avantAppli2} ></div>
-     <br></br>
-      <div className="container">
+      <div className={styles.container}>
+        <img className={styles.appareil} src={DocusaurusSvg} />
         <h1 className={styles.appli}>{siteConfig.title}</h1>
         <p className={styles.tag}>{siteConfig.tagline}</p>
+        
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="docs/Preparation/Download">
+            Download
+          </Link>
+        </div>
+     <div className={styles.avantFeature}> <div className={styles.avantAppli2} ></div>
+     <br></br>
+      <img className={styles.icone} src={Icone} />
+          <h1 className={styles.avantFeatureTitre}>Our Services</h1>
+            <p1> Need a Scintigraphy Application?</p1>
+            <p2>  We have developped several tools for image processing in Scintigraphy.</p2>
       </div>
-    </header>
+      <HomepageFeatures />
+    </div>
+  
   );
 }
 
@@ -26,15 +42,9 @@ export default function Home() {
   return (
     <Layout
       title={`Home ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head/>">
-      <HomepageHeader />
+      description="Description will go into a meta tag in <head />">
       <main>
-      <div className={styles.avantFeature}>
-          <h1>Our Services</h1>
-          <p>Need a Scintigraphy Application?</p>
-          <p>We have developped several tools for image processing in Scintigraphy.</p>
-      </div>
-        <HomepageFeatures />
+      <HomepageHeader />
       </main>
     </Layout>
   );
