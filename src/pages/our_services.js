33f17c1b17badf1db8d250273f0968a0cd38f081
Helyion <gaelbellanger283@gmail.com>
2022-05-03 15:29:40 +0200

    ajout de block de texte

--- src/pages/our_services.js
+++ src/pages/our_services.js
@@ -22,19 +22,31 @@ export default function Home() {
       description="pages about our services">
       <HomepageHeader />
       <main>
-        <div className={styles.text_para_pos}>
-          <h1>Easy to install</h1>
-          <p>Quick and easy to install in just a few clicks on your PC...</p>
-          <h1>Useful everyday applications</h1>
-          <p>Many applications available...</p>
-          <h1>Original tools</h1>
-          <p>Some tools are not available on the imaging consoles...</p>
-          <h1>Updated applications according to the latest guidelines</h1>
-          <p>Tools developed and updated according to the latest recommendations for each type of imagery...</p>
-          <h1>Available in Open source!</h1>
-          <p>It's free!</p>
-          <h1>Possible evolutions and new tools to be developed!</h1>
-          <p>Don't hesitate to come to us for ideas !</p>
+        <div className={styles.text_our_services}>
+          <div className={styles.text_right}>
+            <h1>Easy to install</h1>
+            <p>Quick and easy to install in just a few clicks on your PC...</p>
+          </div>
+          <div className={styles.text_left}>
+            <h1>Useful everyday applications</h1>
+            <p>Many applications available...</p>
+          </div>
+          <div className={styles.text_right}>
+            <h1>Original tools</h1>
+            <p>Some tools are not available on the imaging consoles...</p>
+          </div>
+          <div className={styles.text_left}>
+            <h1>Updated applications according to the latest guidelines</h1>
+            <p>Tools developed and updated according to the latest recommendations for each type of imagery...</p>
+          </div>
+          <div className={styles.text_right}>
+            <h1>Available in Open source!</h1>
+            <p>It's free!</p>
+          </div>
+          <div className={styles.text_left}>
+            <h1>Possible evolutions and new tools to be developed!</h1>
+            <p>Don't hesitate to come to us for ideas !</p>
+          </div>
         </div>
       </main>
     </Layout>
